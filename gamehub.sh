6fc908595c94361227c9d3631f628c278a818f46
Star_destroyer12 <167476043+Star-destroyer12@users.noreply.github.com>
2025-12-02 08:18:28 -0700

    Delete gamehub.sh

--- gamehub.sh
+++
@@ -1,7 +0,0 @@
-#!bin/bash
-PLUGIN_NAME="Gamehub"
-PLUGIN_FUNCTION="Opens a menu with shell based games"
-PLUGIN_DESCRIPTION="Opens a menu with shell based games"
-PLUGIN_AUTHOR="Star_destroyer11"
-PLUGIN_VERSION=1
-
